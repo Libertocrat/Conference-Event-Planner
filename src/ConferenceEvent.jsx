4caea667ed33a26f323f5edd929f96474fdeafd2
Libertocrat <libertocrat@proton.me>
2024-08-13 21:15:38 -0400

    Task 6 - Display selected items in a table

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -5,6 +5,42 @@ import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
 import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
  
+const ItemsDisplay = ({ items }) => {
+    console.log(items);
+    return <>
+        <div className="display_box1">
+            {items.length === 0 && <p>No items selected</p>}
+            <table className="table_item_data">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Unit Cost</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.name}</td>
+                            <td>${item.cost}</td>
+                            <td>
+                                {item.type === "meals" || item.numberOfPeople
+                                ? ` For ${numberOfPeople} people`
+                                : item.quantity}
+                            </td>
+                            <td>{item.type === "meals" || item.numberOfPeople
+                                ? `${item.cost * numberOfPeople}`
+                                : `${item.cost * item.quantity}`}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    </>
+};
+
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
@@ -54,6 +90,29 @@ const ConferenceEvent = () => {
 
     const getItemsFromTotalCost = () => {
         const items = [];
+        venueItems.forEach((item) => {
+        if (item.quantity > 0) {
+            items.push({ ...item, type: "venue" });
+        }
+        });
+        avItems.forEach((item) => {
+        if (
+            item.quantity > 0 &&
+            !items.some((i) => i.name === item.name && i.type === "av")
+        ) {
+            items.push({ ...item, type: "av" });
+        }
+        });
+        mealsItems.forEach((item) => {
+        if (item.selected) {
+            const itemForDisplay = { ...item, type: "meals" };
+            if (item.numberOfPeople) {
+            itemForDisplay.numberOfPeople = numberOfPeople;
+            }
+            items.push(itemForDisplay);
+        }
+        });
+        return items;
     };
 
     const items = getItemsFromTotalCost();
@@ -83,6 +142,11 @@ const ConferenceEvent = () => {
     const venueTotalCost = calculateTotalCost("venue");
     const avTotalCost = calculateTotalCost("av");
     const mealsTotalCost = calculateTotalCost("meals");
+    const totalCosts = {
+        venue: venueTotalCost,
+        av: avTotalCost,
+        meals: mealsTotalCost,
+    };
 
     const navigateToProducts = (idType) => {
         if (idType == '#venue' || idType == '#addons' || idType == '#meals') {
